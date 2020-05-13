5a7ceb73732189cee9cdbdc166e28740e663bd2c
durrante <64601521+durrante@users.noreply.github.com>
2020-05-13 13:35:23 +0100

    Create SyncNewAutoPilotComputersandUsersToAAD.ps1

---
+++ ADConnect/SyncNewAutoPilotComputersandUsersToAAD.ps1
@@ -0,0 +1,7 @@
+##Triggers an ADConnect Delta Sync if new objects are found to be have been created in the OU's in question, this is helpful with Hybrid AD joined devices via Autopilot and avoids the 3rd authenication prompt, change the OU's to match your environment
+$time = [DateTime]::Now.AddMinutes(-5)
+$computers = Get-ADComputer -Filter 'Created -ge $time' -SearchBase "OU=Autopilot Domain Join,OU=Devices,DC=alexdu,DC=co,DC=uk" -Properties Created
+$users = Get-ADUser -Filter 'Created -ge $time' -SearchBase "OU=Accounts,DC=alexdu,DC=co,DC=uk" -Properties Created
+if (($null -ne $computers) -or ($null -ne $users)) {
+    Start-AdSyncSyncCycle
+}
