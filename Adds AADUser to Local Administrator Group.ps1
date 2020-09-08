cb4b43370c793d9c9ae6458ab07e91422a2e615a
durrante <64601521+durrante@users.noreply.github.com>
2020-09-08 11:31:09 +0100

    Update Adds AADUser to Local Administrator Group.ps1

--- Adds AADUser to Local Administrator Group.ps1
+++ Adds AADUser to Local Administrator Group.ps1
@@ -1,2 +1,1 @@
-﻿Add-LocalGroupMember -Group "Administrators" -Member "AzureAD\Timothy.Munn@rpc-tyche.com"
-Add-LocalGroupMember -Group "Administrators" -Member "AzureAD\tmunnadm@rpc-tyche.com"
+﻿Add-LocalGroupMember -Group "Administrators" -Member "AzureAD\Joe.Blogs@primaryazuretenantname.com"
