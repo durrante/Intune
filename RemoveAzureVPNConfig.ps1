6aa699b5d8b57d8b428ff579d06cf2f1c51f9d52
durrante <64601521+durrante@users.noreply.github.com>
2020-11-11 15:37:00 +0000

    Add files via upload

---
+++ RemoveAzureVPNConfig.ps1
@@ -0,0 +1,3 @@
+﻿stop-process -Name AzVpnAppx -ErrorAction SilentlyContinue
+Remove-Item "$env:userprofile\AppData\Local\Packages\Microsoft.AzureVpn_8wekyb3d8bbwe\LocalState\azurevpnconfig.xml" -Force -ErrorAction SilentlyContinue
+Remove-Item "$env:userprofile\AppData\Local\Packages\Microsoft.AzureVpn_8wekyb3d8bbwe\LocalState\rasphone.pbk" -Force -ErrorAction SilentlyContinue
---
+++ Win32_DetectionScript_AzureVPN.ps1
@@ -0,0 +1,3 @@
+﻿if (Test-Path "$env:userprofile\AppData\Local\Packages\Microsoft.AzureVpn_8wekyb3d8bbwe\LocalState") {
+    Write-Host "Found it!"
+}
