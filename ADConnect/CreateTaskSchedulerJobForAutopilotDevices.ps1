7b5f15c14e5844f2410cd8eaeb787b9b41f89ad2
durrante <64601521+durrante@users.noreply.github.com>
2021-06-11 15:04:38 +0100

    Add files via upload

---
+++ ADConnect/CreateTaskSchedulerJobForAutopilotDevices.ps1
@@ -0,0 +1,7 @@
+﻿$credential = Get-Credential
+$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-NoProfile -WindowStyle Hidden -command "& {.\Sync-NewAutopilotComputerstoAAD.ps1}"' -WorkingDirectory "C:\Scripts\"
+$trigger = New-ScheduledTaskTrigger -Daily -At 12am
+$task = Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Sync-NewAutopilotComputerstoAAD" -Description "Monitors an OU for computers created in the last 5 minutes, and forces a sync to AAD" -User $credential.UserName -Password $credential.GetNetworkCredential().Password
+$task.Triggers.Repetition.Interval = "PT5M"
+$task.Triggers.Repetition.Duration = "PT24H"
+$task | Set-ScheduledTask -User $credential.UserName -Password $credential.GetNetworkCredential().Password
