2146d7d3ca816a65ca84ee21c3f350262daad5e7
durrante <64601521+durrante@users.noreply.github.com>
2023-01-18 10:18:33 +0000

    Create Update-HybridADClientScheduledTaskTrigger.ps1

---
+++ UpdateHybridADJoinScheduledTask/Update-HybridADClientScheduledTaskTrigger.ps1
@@ -0,0 +1,25 @@
+<#
+Version: 1.0
+Author:  Alex Durrant
+Script:  Update-HybridADClientScheduledTaskTrigger.ps1
+Date:    18/01/2023
+Description:
+Reconfigures the built-in Automatic-Device-Join scheduled task trigger for user logon only from 1 minute delay to 2 minutes if it exists, this is used for Hybrid AD join.
+Release notes:
+Version 1.0: Original published version.
+The script is provided "AS IS" with no warranties.
+#>
+
+$task = "Automatic-Device-Join"
+$taskExists = Get-ScheduledTask | Where-Object {$_.TaskName -like $task}
+$taskName = $taskExists
+
+if($taskExists) {
+    Write-output "$Task scheduled task is being reconfigured..."
+    $RepeatingTrigger = $Taskname.Triggers[0]
+    $RepeatingTrigger.Delay = "PT2M"
+    Set-ScheduledTask -InputObject $TaskName
+} else {
+   Write-output "$Task scheduled task does NOT exist on this device..."
+   
+}
